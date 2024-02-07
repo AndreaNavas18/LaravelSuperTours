d0d9f083569cde34dd57bdfdf94fc29bcf0c001f
andrea navas <karennavas333@gmail.com>
2024-02-07 08:12:12 -0500

    7cc

--- resources/views/section2.blade.php
+++ resources/views/section2.blade.php
@@ -1,19 +1,22 @@
 <section class="section2">
     <div class="divSection2">
         <div class="card1S2">
-            <h2>Transportation</h2>
-            <p>Descripcion</p>
-            <button>Book now</button>
+            <h2 class="h2S2">TRANSPORTATION</h2>
+            <p>"Travel in style and comfort! Explore amazing 
+                destinations with our premium transportation service."</p>
+            <button class="btnBN">Book now</button>
         </div>
         <div class="card2S2">
-            <h2>One day tour</h2>
-            <p>Descripcion</p>
-            <button>Book now</button>
+            <h2 class="h2S2">ONE DAY TOUR</h2>
+            <p> "Exciting day-long adventures! Discover 
+                the best of every destination with our one day tours."</p>
+            <button class="btnBN2">Book now</button>
         </div>
         <div class="card3S2">
-            <h2>Multiday tour</h2>
-            <p>Descripcion</p>
-            <button>Book now</button>
+            <h2 class="h2S2">MULTIDAY TOUR</h2>
+            <p> "Explore without limits! Immerse yourself 
+                in unforgettable experiences with our multiday tours."</p>
+            <button class="btnBN">Book now</button>
         </div>
     </div>
     <div class="div1Section2">
