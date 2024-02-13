aec1f0a8787a127ff8383ba02f19396cf7fbbd1b
andrea navas <karennavas333@gmail.com>
2024-02-13 09:32:55 -0500

    13021302x

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,22 +12,52 @@
 <body>
     @include('header2')
     <div class="containerCU">
-        <h1>Contact Us</h1>
+        <h1 class="tituloCN">Contact Us</h1>
         <section class="sectionCU">
-            <form action="">
-                <label for="reason">Contact Reason</label>
-                <select name="reason" id="reason">
-                    <option value="general">General</option>
-                    <option value="reservation">Reservation</option>
-                    <option value="complaint">Complaint</option>
-                    <option value="other">Other</option>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required>
-                <label for="email">Email:</label>
-                <input type="email" id="email" name="email" required>
-                <label for="message">Message:</label>
-                <textarea id="message" name="message" required></textarea>
-                <input type="submit" value="Submit">
+            <form action="" class="formClass">
+                <div class="rowsCN">
+                    <label for="reason" >
+                        <h3 class="labelCN">
+                            Contact Reason:
+                        </h3>
+                    </label>
+                    <select class="selectCN" name="reason" id="reason">
+                        <option value="null">Select an option</option>
+                        <option value="general">General</option>
+                        <option value="reservation">Reservation</option>
+                        <option value="complaint">Complaint</option>
+                        <option value="other">Other</option>
+                    </select>
+                </div>
+                <div class="rowsCN"> 
+                    <label for="name" class="labelCN">
+                        <h3 class="labelCN">
+                            Name:
+                        </h3>
+                    </label>
+                    <input type="text" id="name" name="name" class="inputCN" required>
+                </div>
+                <div class="rowsCN"> 
+                    <label for="email" class="labelCN">
+                        <h3 class="labelCN">
+                           Email:
+                        </h3>
+                    </label>
+                    <input type="email" id="email" name="email" class="inputCN" required>
+                </div>
+                <div class="rowsCN"> 
+                    <label for="message" class="labelCN">
+                        <h3 class="labelCN">
+                            Message:
+                        </h3>
+                    </label>
+                    <textarea id="message" name="message" class="textACN" required></textarea>
+                </div>
+                <button type="submit">
+                    <h3 class="buttonCN">
+                        Send
+                    </h3
+                </button>
             </form>
 
         </section>
